e836ee0f83e64d44fdf78a2602b8c958f23f0cca
BrennanKnopf <brennanknopf@gmail.com>
2022-02-21 10:09:24 -0600

    Fixed an error in my view.py with the http bad request error

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -41,7 +41,7 @@ class SongDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         song = self.get_object(pk)
